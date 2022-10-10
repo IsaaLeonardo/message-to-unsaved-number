bfda0703d8c08f3d0688df035b7b098927379a83
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-10-10 18:52:37 -0400

    URL opening by user entering

--- open-chat.js
+++ open-chat.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let phoneNumber = document.getElementById('number')
     let btn = document.getElementById('button');
+
     btn.addEventListener('click', function(activeTab){
-        let newURL = "https://wa.me/+584141803713";
+        let numberToChat = phoneNumber.value
+
+        let newURL = "https://wa.me/+58" + numberToChat
+        // alert(newURL)
         chrome.tabs.create({ url: newURL })
     });
 });
