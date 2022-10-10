8b6de1c6a4abc37e3f7b365bdc94b1431d2e095c
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-10-10 19:03:14 -0400

    Add country-code input form

--- open-chat.js
+++ open-chat.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let countryCode = document.getElementById('country-code')
     let phoneNumber = document.getElementById('number')
     let btn = document.getElementById('button');
 
     btn.addEventListener('click', function(activeTab){
-        let numberToChat = phoneNumber.value
-
-        let newURL = "https://wa.me/+58" + numberToChat
+        let newURL = "https://wa.me/+" + countryCode.value + phoneNumber.value
         // alert(newURL)
         chrome.tabs.create({ url: newURL })
     });
