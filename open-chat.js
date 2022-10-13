6c8e0f266c75fa0d1eb3931acdfae21061c9e14f
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-10-13 17:20:15 -0400

    creating the function open-chat in a different file to popup.js

--- open-chat.js
+++ open-chat.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let countryCode = document.getElementById('country-code')
-    let phoneNumber = document.getElementById('number')
-    let btn = document.getElementById('button');
-
-    btn.addEventListener('click', function(activeTab){
-        let newURL = "https://wa.me/+" + countryCode.value + phoneNumber.value
-        // alert(newURL)
-        chrome.tabs.create({ url: newURL })
-    });
-});
+function openChat(countryCode, phoneNumber) {
+    let newURL = "https://wa.me/+" + countryCode + phoneNumber
+    chrome.tabs.create({ url: newURL })
+}
